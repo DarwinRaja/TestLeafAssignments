dc4e9ac94b9c377eca1961569e5d615755f33635
DarwinRaja <31180183+DarwinRaja@users.noreply.github.com>
2025-03-02 13:45:51 +0530

    Added Day1 assignments

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			float version = 91.0f;
+			String developer = "Google";
+			boolean isBeta = false;
+			int releaseYear = 2008;
+			char shortcutKey = 'C';
+			System.out.println("The browser Chrome is developed by"+" "+developer+" "+"and released in the year"+" "+releaseYear);
+			System.out.println("Your current version is:"+" "+version);
+			System.out.println("Are you using the beta version?"+" "+isBeta);
+			System.out.println("It can be accessed using"+" "+shortcutKey+" "+"shortcut key in Desktop");
+	}
+
+}
---
+++ week1/day1/Firefox.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			float browserVersion = 100.2f;
+			String browserName = "firefox";
+			boolean isVisible = true;
+			int releaseYear = 1998;
+			char browserLogo = 'f';
+			System.out.println("The browser"+" "+browserName+" "+"released in"+" "+releaseYear+" "+"with version"+" "+browserVersion+" "+"has browser logo"+" "+browserLogo);
+			System.out.println("Is browser shortcut visible in Desktop?"+" "+isVisible);
+	}
+
+}
